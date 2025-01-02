603fdf4ad5e368e8f879fc85b5c163e8e92cff19
joshmetzger <75275288+joshmetzger@users.noreply.github.com>
2025-01-01 16:56:19 -0800

    pet websiteupdated

---
+++ Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, Owrld!';
+console.log(greeting);
+
+var greeting2 = "Welcome to Nod.js";
+console.log(greeting2);
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentence1);
